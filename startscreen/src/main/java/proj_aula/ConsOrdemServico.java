50a14938e6334d3be4a36fbfd724cc03c5081de9
juan <36975161+jucsamp@users.noreply.github.com>
2018-03-27 20:53:12 -0300

    Add files via upload

--- startscreen/src/main/java/proj_aula/ConsOrdemServico.java
+++ startscreen/src/main/java/proj_aula/ConsOrdemServico.java
@@ -2,39 +2,68 @@ package proj_aula;
 
 import java.awt.Container;
 
-import javax.swing.JInternalFrame;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
 
 public class ConsOrdemServico extends JInternalFrame{
-        JButton limpar, cad;
-	JLabel nome;
-	JTextField txtnome;
+        JButton fechar, pesquisar;
+	JLabel pesqpor;
+	JComboBox parpesq;
+	String campos[] = {"Código do Funcionário", "Código do Cliente", "Código do Veículo", "Descrição do problema", "Observações Adicionais", "Data de Entrada", "Data de Finalização", "Valor total: R$", "Código do produto ou serviço"};
+        JTextField pesq_txt;
+	ImageIcon pesq;
 	public ConsOrdemServico() {
-		super("Consulta de Funcionários");  
+		super("Consulta de Ordens de Serviço");
 		Container tela = getContentPane();
 		setLayout(null);
 		
-		limpar = new JButton("Limpar");
-		cad = new JButton("Cadastrar");
-		nome = new JLabel("Nome");
-		txtnome = new JTextField(50);
+		parpesq = new JComboBox(campos);
+		parpesq.setMaximumRowCount(4);
+		parpesq.setFont(new Font("Century Gothic", 0, 14));
+		parpesq.setBackground(Color.WHITE);
 		
-		nome.setBounds(30,30,50,20);
-		cad.setBounds(50,60,100,20);
-		limpar.setBounds(50,90,100,20);
-		txtnome.setBounds(80,30,50,20);
+		pesquisar = new JButton("");
+		pesq_txt = new JTextField(25);
+		fechar = new JButton("Fechar");
+		pesqpor = new JLabel("Pesquisa por:");
+		pesq = new ImageIcon("search.png");
+		pesq.setImage(pesq.getImage().getScaledInstance(20,20,100));
+		pesquisar.setIcon(pesq);
 		
-		tela.add(cad);
-		tela.add(limpar);
-		tela.add(nome);
-		tela.add(txtnome);
+		pesquisar.setBackground(Color.WHITE);
+		pesq_txt.setFont(new Font("Century Gothic", 0, 14));
+		pesqpor.setFont(new Font("Century Gothic", 1, 14));
+		fechar.setFont(new Font("Century Gothic", 1, 15));
+		fechar.setBackground(Color.WHITE);
 		
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		setSize(250,250);
+		pesquisar.setBounds(275,40,30,20);
+		pesq_txt.setBounds(10,40,250,20);
+		pesqpor.setBounds(10,10,100,20);
+		parpesq.setBounds(100,10,205,20);
+		fechar.setBounds(10,190,295,20);
+		
+		fechar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int status = JOptionPane.showConfirmDialog(null,"Deseja realmente fechar a janela? Todos os dados escritos serão apagados.", " ", JOptionPane.YES_NO_OPTION);
+				if(status == JOptionPane.YES_OPTION) {
+					dispose();
+				}
+			}
+		});
+		
+		pesquisar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String val_pesq = pesq_txt.getText();
+			}
+		});
+		
+		tela.add(pesquisar);
+		tela.add(pesq_txt);
+		tela.add(pesqpor);
+		tela.add(parpesq);
+		tela.add(fechar);
+		setSize(325,250);
 		setVisible(true);
-     }
+	}
 }
